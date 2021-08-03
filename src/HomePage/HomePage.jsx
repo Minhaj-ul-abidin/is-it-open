872d1d2519f02119217c99253bd647129da983bc
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2021-08-04 04:21:46 +0530

    move logout option to before list

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,29 +1,31 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { restaurantsActions } from "../_actions";
 import { RestaurantList } from "../_components";
 
 function HomePage() {
-    const restaurants = useSelector((state) => state.restaurants);
+  const restaurants = useSelector((state) => state.restaurants);
 
-    const user = useSelector(state => state.authentication.user);
-    console.log({ user });
-    const dispatch = useDispatch();
+  const user = useSelector((state) => state.authentication.user);
+  console.log({ user });
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(restaurantsActions.get());
-    }, []);
+  useEffect(() => {
+    dispatch(restaurantsActions.get());
+  }, []);
 
-    return (
-      <div className="col-lg-8 offset-lg-2">
-        <h1>Hi {user.name}!</h1>
-        <br />
-        <h3> List of Restaurants </h3>
-        <RestaurantList restaurants={restaurants} />
+  return (
+    <div className="col-lg-8 offset-lg-2">
+      <h1>Hi {user.name}!</h1>
+      <p>
         <Link to="/login">Logout</Link>
-      </div>
-    );
+      </p>
+      <br />
+      <h3> List of Restaurants </h3>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  );
 }
 
 export { HomePage };
